d883fafc4ebcf336d1a52ce68bedadb87cdb815d
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-03-21 23:53:05 -0400

    added data models

---
+++ cloud-run/golang/shared/models/Click.go
@@ -0,0 +1,19 @@
+package Linkwire.Models
+
+type Click struct {
+	ClickID   string `json:"clickID" firestore:"clickID"`
+	IP        string `json:"ip" firestore:"ip"`
+	LinkID    string `json:"linkID" firestore:"linkID"`
+	Timestamp int64  `json:"timestamp" firestore:"timestamp"`
+	UserAgent string `json:"userAgent" firestore:"userAgent"`
+	OS        string `json:"os" firestore:"os"`
+	Client    string `json:"client" firestore:"client"`
+	Device    string `json:"device" firestore:"device"`
+	Location  string `json:"location" firestore:"location"`
+	ISP       string `json:"isp" firestore:"isp"`
+	Mobile    bool   `json:"mobile" firestore:"mobile"`
+	Proxy     bool   `json:"proxy" firestore:"proxy"`
+	Hosting   bool   `json:"hosting" firestore:"hosting"`
+	ASN       string `json:"asn" firestore:"asn"`
+	FirestoreID string
+}
---
+++ cloud-run/golang/shared/models/User.go
@@ -0,0 +1,10 @@
+package Linkwire.Models
+
+type User struct {
+	Username string `json:"username" firestore:"username"`
+	Email    string `json:"email" firestore:"email"`
+	Password string `json:"password" firestore:"password"`
+	PremiumUser bool   `json:"premiumUser" firestore:"premiumUser"`
+	Links string[] `json:"links" firestore:"links"`
+	FirestoreID string
+}
