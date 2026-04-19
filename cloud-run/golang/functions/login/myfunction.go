d192499d124c649eefa221dd3edce7343b825710
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-04-19 01:35:59 -0400

    fixed content type

--- cloud-run/golang/functions/login/myfunction.go
+++ cloud-run/golang/functions/login/myfunction.go
@@ -65,5 +65,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	
 	json.NewEncoder(w).Encode(user)
 }
