341c00bc8fcfcba62c9f289120d9d133bad69547
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-04-19 02:00:10 -0400

    updated to exclude password from JSON response

--- cloud-run/golang/shared/models/User.model.go
+++ cloud-run/golang/shared/models/User.model.go
@@ -3,7 +3,7 @@ package myfunction
 type User struct {
 	Username string `json:"username" firestore:"username"`
 	Email    string `json:"email" firestore:"email"`
-	Password string `json:"password" firestore:"password"`
+	Password string `json:"-" firestore:"password"`
 	PremiumUser bool   `json:"premiumUser" firestore:"premiumUser"`
 	Links []string `json:"links" firestore:"links"`
 
