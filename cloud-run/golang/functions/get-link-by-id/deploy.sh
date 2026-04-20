bcc246a1f4bf4c1f595f1651b8ab469be382c7a7
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-04-20 00:35:41 -0400

    implemented GetLinkById function

---
+++ cloud-run/golang/functions/get-link-by-id/deploy.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+gcloud functions deploy get-link-by-id \
+  --gen2 \
+  --region=$GCP_REGION \
+  --runtime=go126 \
+  --source=. \
+  --entry-point=Handler \
+  --trigger-http \
+  --set-env-vars PROJECT_ID=$GCP_PROJECT_NAME,JWT_SECRET_KEY=$JWT_SECRET_KEY \
+  --allow-unauthenticated
---
+++ cloud-run/golang/functions/get-link-by-id/myfunction.go
@@ -0,0 +1,83 @@
+package myfunction
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"slices"
+	"strings"
+
+	"github.com/AjayBhatta16/linkwire-golang-shared/constants"
+	"github.com/AjayBhatta16/linkwire-golang-shared/models"
+    "github.com/AjayBhatta16/linkwire-golang-shared/utilities"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// validate request
+	id := GetLinkIDFromPath(r)
+
+	if id == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	// validate JWT
+	token := utilities.GetTokenFromCookies(w, r)
+
+	notExpired, expErr := utilities.ValidateJWTNotExpired(token)
+
+	if expErr != nil || !notExpired {
+		log.Println("Handler - JWT is expired or invalid:", expErr)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	tokenUsername, err := utilities.GetJWTUsername(token)
+
+	if err != nil {
+		log.Println("Handler - Error validating JWT:", err)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	// fetch link by id
+	data, err2 := utilities.GetItemsByFieldValue[models.Link, *models.Link](constants.LINK_CONTAINER_NAME, "trackingID", id)
+
+	if err2 != nil {
+		log.Println("Handler - Error fetching link by id:", err2)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if len(data) == 0 {
+		http.Error(w, "Link not found", http.StatusNotFound)
+		return
+	}
+
+	// validate link
+	link := data[0]
+
+	if link.CreatedBy != tokenUsername {
+		log.Printf("Handler - JWT username %s does not match link's createdBy %s", tokenUsername, link.CreatedBy)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	// return link as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(link)
+}
+
+func GetLinkIDFromPath(r *http.Request) string {
+	url := r.URL.Path
+	
+	tokens := strings.Split(url, "/")
+	slices.Reverse(tokens)
+	lastToken := tokens[0]
+
+	if lastToken == "links" || lastToken == "links/" {
+		return ""
+	}
+
+	return lastToken
+}
