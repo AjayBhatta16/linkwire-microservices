7e00a9810aa05481e7f9ae631d9fa27bf92e7acc
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-05-26 01:10:52 -0400

    Added GetDeviceInfo microservice

---
+++ cloud-run/nodejs/functions/get-device-info/deploy.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+gcloud functions deploy get-device-info \
+  --gen2 \
+  --region=$GCP_REGION \
+  --runtime=nodejs24 \
+  --source=. \
+  --entry-point=handler \
+  --trigger-http \
+  --set-env-vars PROJECT_ID=$GCP_PROJECT_NAME,JWT_SECRET_KEY=$JWT_SECRET_KEY \
+  --allow-unauthenticated
---
+++ cloud-run/nodejs/functions/get-device-info/index.js
@@ -0,0 +1,29 @@
+const functions = require("@google-cloud/functions-framework");
+const deviceDetector = require("node-device-detector");
+
+functions.http("handler", async (req, res) => {
+    const data = parseRequest(req.body);
+
+    if (data === null) {
+        res.status(400).send("Bad Request: missing or invalid request data");
+    }
+
+    const { userAgent } = data;
+
+    if (!userAgent) {
+        res.status(400).send("Bad Request: missing userAgent field");
+    }
+
+    let detectedAgent = deviceDetector.detect(req.body.userAgent);
+
+    res.status(200).json({
+        type: detectedAgent.device.type,
+        brand: detectedAgent.device.brand,
+        model: detectedAgent.device.model,
+        osName: detectedAgent.os.name,
+        osVersion: detectedAgent.os.version,
+        clientType: detectedAgent.client.type,
+        clientName: detectedAgent.client.name,
+        clientVersion: detectedAgent.client.version,
+    });
+});
